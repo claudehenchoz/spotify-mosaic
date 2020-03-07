a9d57e484b97734650a76ac916ffe9aa1dadfed9
Claude Henchoz <claude.henchoz@gmail.com>
2020-03-07 18:54:40 +0100

    Cosmetics

--- spotify-mosaicgen.py
+++ spotify-mosaicgen.py
@@ -1,21 +1,19 @@
+import os
 import spotipy
-import spotipy.util as util
-from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.oauth2 as oauth2
-import json
 from PIL import Image
 from PIL import ImageDraw
 import requests
 from io import BytesIO
 from dotenv import load_dotenv
-import os
+load_dotenv()
 
 
-market     = "US"
-width      = 320
-height     = 320
-rows       = 5
-columns    = 8
+market = "US"
+width = 320
+height = 320
+rows = 5
+columns = 8
 
 # gets vars
 CLIENT_ID = os.getenv("CLIENT_ID")
@@ -32,14 +30,17 @@ token = credentials.get_access_token()
 spotify = spotipy.Spotify(auth=token)
 
 # gets all playlist items (we have to get all to produce reverse order list)
-results = spotify.user_playlist_tracks(SPOTIFY_PLAYLIST_USER, SPOTIFY_PLAYLIST_ID, limit=100)
+results = spotify.user_playlist_tracks(
+            SPOTIFY_PLAYLIST_USER,
+            SPOTIFY_PLAYLIST_ID,
+            limit=100)
 tracks = results['items']
 while results['next']:
     results = spotify.next(results)
     tracks.extend(results['items'])
 
 # get album art for newest n tracks
-img_list   = []
+img_list = []
 for track in tracks[-(rows*columns):]:
     response = requests.get(track["track"]["album"]["images"][0]["url"])
     img = Image.open(BytesIO(response.content))
@@ -51,17 +52,17 @@ for image in img_list:
 columns = int(len(img_list)/rows)
 
 # creates a new empty image, RGB mode
-mosaic = Image.new('RGB',(columns* width,rows*height))
+mosaic = Image.new('RGB', (columns * width, rows*height))
 
 # prepares draw and font objects to render text
-draw   = ImageDraw.Draw(mosaic)
+draw = ImageDraw.Draw(mosaic)
 
 # creates mosaic
-k=0
-for j in range( 0, rows * height, height ):
-    for i in range( 0, columns * width, width ):
+k = 0
+for j in range(0, rows * height, height):
+    for i in range(0, columns * width, width):
         # paste the image at location i,j:
-        mosaic.paste( img_list[k], (i,j) )
+        mosaic.paste(img_list[k], (i, j))
         # Select next image and text
         k = k + 1
 
